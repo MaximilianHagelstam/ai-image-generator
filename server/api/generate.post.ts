a4df4b3e382022db662bd6800ad1f363e89bb9ff
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-03-17 11:12:24 +0200

    refactor: combine post and get endpoints into one

--- server/api/generate.post.ts
+++ server/api/generate.post.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default defineEventHandler(async (event) => {
   const body = (await readBody(event)) as { prompt: string | undefined };
 
@@ -9,16 +11,16 @@ export default defineEventHandler(async (event) => {
   }
 
   if (process.env.NODE_ENV !== 'production') {
-    return { id: 'mockId1234' };
+    return {
+      url: 'https://pbxt.replicate.delivery/YXbcLudoHBIYHV6L0HbcTx5iRzLFMwygLr3vhGpZI35caXbE/out-0.png',
+    };
   }
 
-  const response = await $fetch('https://api.replicate.com/v1/predictions', {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${process.env.REPLICATE_API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
+  const { data: postData, status: postStatus } = await axios.post<{
+    id: string;
+  }>(
+    'https://api.replicate.com/v1/predictions',
+    {
       version:
         '39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b',
       input: {
@@ -36,8 +38,37 @@ export default defineEventHandler(async (event) => {
         prompt_strength: 0.8,
         num_inference_steps: 25,
       },
-    }),
+    },
+    {
+      headers: {
+        Authorization: `Token ${process.env.REPLICATE_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  if (postStatus !== 201) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error generating image',
+    });
+  }
+
+  const { data: getData, status: getStatus } = await axios.get<{
+    results: [{ output: string[] }];
+  }>(`https://api.replicate.com/v1/predictions?id=${postData.id}`, {
+    headers: {
+      Authorization: `Token ${process.env.REPLICATE_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
   });
 
-  return response;
+  if (getStatus !== 200) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error retrieving image',
+    });
+  }
+
+  return { url: getData.results[0].output[0] };
 });
