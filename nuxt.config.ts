f7b748d331a8df1c6b35be6678d94a47d5398bd6
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-03-17 11:29:55 +0200

    Add rate limiting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-security'],
   googleFonts: {
     families: {
       Inter: '100..900',
@@ -17,6 +17,18 @@ export default defineNuxtConfig({
   },
   typescript: {
     strict: true,
+  },
+  routeRules: {
+    '/api/generate': {
+      security: {
+        rateLimiter: {
+          tokensPerInterval: 3, // an IP address can make 3 req / 24 hrs
+          interval: 24 * 60 * 60 * 1_000, // 24 hrs
+          headers: false,
+          throwError: true,
+        },
+      },
+    },
   },
   devtools: { enabled: false },
 });
