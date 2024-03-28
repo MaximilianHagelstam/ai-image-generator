a91cea93fad451b8702dc3284a88d7629f05870f
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-03-28 22:25:10 +0200

    test: add coverage

--- test/generate.test.ts
+++ test/generate.test.ts
@@ -1,13 +1,9 @@
 import { $fetch, setup } from '@nuxt/test-utils';
 import { describe, expect, it } from 'vitest';
 
-describe('example', async () => {
+describe('landing page', async () => {
   await setup({
     server: true,
-  });
-
-  it('Renders Stxl AI', async () => {
-    expect(await $fetch('/')).toContain('Stxl AI');
   });
 
   it('Renders Stxl AI', async () => {
